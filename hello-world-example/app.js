4ddc84588f2b9092fbe176b2bfef2c9ecf604268
Edwin Rivera <waehake@gmail.com>
2017-09-05 22:06:22 -0400

    updated first example

--- hello-world-example/app.js
+++ hello-world-example/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('This works');
+  res.send('This works!');
 });
 
 app.listen(3000, function() {
