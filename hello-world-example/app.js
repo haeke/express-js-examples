1c12cd5f5103aac548baabdc2b0458f81d6d01ce
Edwin Rivera <waehake@gmail.com>
2017-09-05 21:59:58 -0400

    added the hello-world example

---
+++ hello-world-example/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('This works');
+});
+
+app.listen(3000, function() {
+  console.log('this example is listening on port 3000');
+});
