122153b141b0427a761f7c44527dd247b9d7d388
Edwin Rivera <waehake@gmail.com>
2017-09-10 18:11:38 -0400

    added an about route

--- express-generator-example/myapp/routes/index.js
+++ express-generator-example/myapp/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express Example' });
+});
+
+router.get('/about', (req, res) => {
+  // res.send('test this works');
 });
 
 module.exports = router;
